8f324c052d3e6a6d76fd54b5ef23a05ad43d935c
Finn Kristian Meland <1390752+fkmeland@users.noreply.github.com>
2026-09-17 10:55:49 +0200

    feat: use configTzTime when WIFI_TZ_INFO is defined for NTP configuration

--- lib/wifiModule/wifiModule.cpp
+++ lib/wifiModule/wifiModule.cpp
@@ -286,7 +286,11 @@ static void applyConnectedSideEffects()
   if (!wifiNtpConfigured)
   {
     sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+#ifdef WIFI_TZ_INFO
+    configTzTime(WIFI_TZ_INFO, "pool.ntp.org", "time.nist.gov", "time.google.com");
+#else
     configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org", "time.nist.gov", "time.google.com");
+#endif
     wifiNtpConfigured = true;
   }
 
@@ -360,7 +364,11 @@ static void maybeLogPendingTime()
     Log.warning(F("[WiFi]: NTP not synced yet (retry %d), re-arming SNTP..." CR), ntpRetryCount);
     sntp_stop();
     sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+#ifdef WIFI_TZ_INFO
+    configTzTime(WIFI_TZ_INFO, "pool.ntp.org", "time.nist.gov", "time.google.com");
+#else
     configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org", "time.nist.gov", "time.google.com");
+#endif
   }
 }
 
